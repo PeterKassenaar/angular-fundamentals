7a2263f9c96347609df5d8ea0b55eb1f54544d49
Peter Kassenaar <info@kassenaar.com>
2026-05-29 14:20:36 +0200

    feat: some dummy changes

--- examples/classic/100-helloworld/src/app/app.component.ts
+++ examples/classic/100-helloworld/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Component, OnInit} from '@angular/core';
   // Add component description/annotations here
   selector: 'hello-world',
   template: `
-    <h1>Hello World!</h1>
+    <h1>Hello World 123!</h1>
     <h2>This is Angular</h2>
     <a href="https://angular.dev" target="_blank">Angular Website</a>
   `
